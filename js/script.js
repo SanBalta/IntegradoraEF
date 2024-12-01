509b72f2662aa8966fef6b49a4470e16908a4765
Christian <gioosan30@hotmail.com>
2024-12-01 01:35:37 -0600

    Se agrega animaciones y archivo js

---
+++ js/script.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+    $(".head_de_imagen").waypoint(function() {
+        $(".head_de_imagen").addClass("animate__animated animate__fadeInLeft");
+    }, {offset:"100%"} );
+    $(".head_de_imagen_2").waypoint(function() {
+        $(".head_de_imagen_2").addClass("animate__animated animate__fadeInLeft");
+    }, {offset:"85%"} );
+    $(".head_de_imagen_3").waypoint(function() {
+        $(".head_de_imagen_3").addClass("animate__animated animate__zoomIn");
+    }, {offset:"75%"} );
+    $(".head_de_imagen_4").waypoint(function() {
+        $(".head_de_imagen_4").addClass("animate__animated animate__fadeInLeft");
+    }, {offset:"65%"} );
+    $(".head_de_imagen_5").waypoint(function() {
+        $(".head_de_imagen_5").addClass("animate__animated animate__zoomIn");
+    }, {offset:"55%"} );
+    $(".head_de_imagen_6").waypoint(function() {
+        $(".head_de_imagen_6").addClass("animate__animated animate__fadeInLeft");
+    }, {offset:"55%"} );
+    $(".head_de_imagen_7").waypoint(function() {
+        $(".head_de_imagen_7").addClass("animate__animated animate__lightSpeedInRight");
+    }, {offset:"80%"} );
+    $(".head_de_imagen_8").waypoint(function() {
+        $(".head_de_imagen_8").addClass("animate__animated animate__zoomInDown");
+    }, {offset:"100%"} );
+
+});
